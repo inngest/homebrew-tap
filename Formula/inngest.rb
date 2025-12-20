2e72b824ea17baa96887b06c93bb2fc911ec2a37
Albert Chae <albertchae@users.noreply.github.com>
2025-12-19 19:16:29 -0800

    Add formula for building from source
    Homebrew formulae are used for building applications from source.
    
    The benefit of this over casks is that we won't need to sign and
    notarize the binary.
    
    The negative is we'd have to maintain an alternative build path (since
    we mainly use goreleaser in inngest/inngest).

---
+++ Formula/inngest.rb
@@ -0,0 +1,38 @@
+class Inngest < Formula
+  desc "Inngest CLI and development server"
+  homepage "https://www.inngest.com/"
+  url "https://github.com/inngest/inngest.git",
+      tag:      "v1.15.1",
+      revision: "196512c9bca5fa8a77e4ea78446094a975096bac"
+  license "See https://github.com/inngest/inngest/blob/main/LICENSE.md"
+  head "https://github.com/inngest/inngest.git", branch: "main" # brew install --build-from-source --formula inngest/tap/inngest --HEAD
+
+  depends_on "go" => :build
+  depends_on "node" => :build
+  depends_on "pnpm" => :build
+
+  def install
+    # Initialize submodules (for embedded docs)
+    system "git", "submodule", "update", "--init", "--recursive"
+
+    # Build the frontend UI (gets embedded in the Go binary)
+    cd "ui/apps/dev-server-ui" do
+      system "pnpm", "install", "--frozen-lockfile"
+      system "pnpm", "build"
+    end
+
+    # Copy built UI to static directory for embedding
+    cp_r "ui/apps/dev-server-ui/dist/.", "pkg/devserver/static/"
+
+    # Prepare version tag
+    short_commit = Utils.safe_popen_read("git", "rev-parse", "--short=7", "HEAD").chomp
+    ldflags = "-s -w -X github.com/inngest/inngest/pkg/inngest/version.Version=#{version} -X github.com/inngest/inngest/pkg/inngest/version.Hash=#{short_commit}"
+
+    # Build the Go binary
+    system "go", "build", *std_go_args(ldflags:, output: bin/"inngest"), "./cmd"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/inngest version")
+  end
+end
